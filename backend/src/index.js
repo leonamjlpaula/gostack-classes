4fc02cb38b84f9df8d6cc5c525f455ea80693e34
Leonam de Paula <leonamjlpaula@gmail.com>
2020-04-26 12:41:49 -0300

    Back-end first application

---
+++ backend/src/index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const {uuid, isUuid} = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(request, response, next){
+  const {method, url} = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log(logLabel);
+  return next();
+}
+
+function validateProjectId(request, response, next){
+  const {id} = request.params;
+  if(!isUuid(id))
+    return response.status(400).json({message: 'Invalid project ID.'})
+
+  return next();
+}
+
+app.use(logRequests)
+app.use('/projects/:id', validateProjectId);
+
+app.get('/projects', (request, response) => {
+  const {title} = request.query;
+
+  const results = title
+    ? projects.filter(p => p.title.includes(title))
+    : projects;
+
+  return response.json(results);
+});
+
+app.post('/projects', (request, response) => {
+  const {title, owner} = request.body;
+
+  const project = {id: uuid(), title, owner};
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+  const {id} = request.params;
+  const {title, owner} = request.body;
+
+  const index = projects.findIndex(p => p.id === id);
+
+  if(index < 0)
+    return response.status(400).json({message: 'Project not found'});
+
+  const project = {
+    id, title, owner
+  };
+
+  projects[index] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  const {id} = request.params;
+
+  const index = projects.findIndex(p => p.id === id);
+
+  if(index < 0)
+    return response.status(400).json({message: 'Project not found'});
+
+
+  projects.splice(index, 1);
+
+  return response.status(204).send();
+});
+
+app.listen(3333)
